1a25ba84ceb9bbbed512bd77522d5c8e42b7e569
xr-vivi <HeyVivi@outlook.sg>
2021-04-21 17:09:07 +0800

    completing guess game scripts | guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+count=1
+files=13
+
+while [[ $count -gt 0 ]]
+do
+	echo "[Guess] How many files in the current directory?"
+	read answer
+
+	if [[ $answer -gt $files ]]
+	then
+		echo "your guess is too high. Please try again!"
+	elif [[ $answer -lt $files ]]
+	then 
+		echo "your guess is too low. Please try again!"
+	let count=$count+1
+fi
+	if [[ $answer -eq $files ]]
+	then
+		echo "Congratulates! Your guess is correct."
+	let count=$count*0
+fi
+done
